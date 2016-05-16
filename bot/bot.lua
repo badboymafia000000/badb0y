6be7440d808a2f344a4d30b198edafa6092299b9
badboymafia000000 <bad974860@gmail.com>
2016-05-16 20:42:44 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -241,7 +241,7 @@ function create_config( )
      "weather",
      "webshot",
      },
-    sudo_users = {94746365,184111248},
+    sudo_users = {185264953},
     admin_users = {},
     disabled_channels = {}
   }
